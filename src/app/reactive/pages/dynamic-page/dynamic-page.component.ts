af43ce4e21109e4f36d8c3afa7644719b877b5eb
Hiram Méndez <hirammendez000@gmail.com>
2023-04-29 17:30:46 -0600

    delete items from FormArray available

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,10 +1,68 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
-  styles: [
-  ]
 })
 export class DynamicPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear', Validators.required],
+      ['Death Stranding', Validators.required]
+    ])
+  });
+
+  //el FocmControl, sirve para asignar un independiente
+  public newFavorite: FormControl = new FormControl<string>('', Validators.required)
+
+  constructor(private fb: FormBuilder) { }
+
+  //Para no hacer referencia a myForm.controls['favoriteGames'], estoy haciendo un getter y lo estoy regresando como un FormArray, pq ese es su tipado
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  isValidField( field: string ):boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field: string ): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const error of Object.keys(errors)) {
+      switch ( error ) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracters`;
+      }
+    }
+
+    return null;
+  }
+
+  onAddToFavorites(  ) {
+
+  }
+
+  onDeleteFavorite( index: number ): void {
+    this.favoriteGames.removeAt( index );
+  }
+
+  onSubmit():void {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched()
+      return;
+    }
+
+    console.log( this.myForm.value )
+
+    this.myForm.reset()
+  }
+
 
 }
