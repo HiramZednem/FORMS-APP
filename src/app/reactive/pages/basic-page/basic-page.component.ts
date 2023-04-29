84fecdec6e5cda724729280fc8a3e31a83b8541c
Hiram Méndez <hirammendez000@gmail.com>
2023-04-29 15:45:10 -0600

    validations for all inputs

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 const rtx5090 = {
   name: 'RTX 5090',
@@ -31,7 +31,28 @@ export class BasicPageComponent implements OnInit {
   constructor( private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    //this.myForm.reset( rtx5090 )
+    this.myForm.reset( rtx5090 )
+  }
+
+  isValidField( field: string ):boolean | null {
+    return this.myForm.controls[field].errors
+     && this.myForm.controls[field].touched
+  }
+
+  getFieldError( field: string ): string | null {
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+    for (const error of Object.keys(errors)) {
+      switch ( error ) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } caracters`;
+      }
+    }
+
+    return null;
   }
 
   onSave(): void {
@@ -39,10 +60,10 @@ export class BasicPageComponent implements OnInit {
       /*
       * Normalmente en los formularios, nosotros en el html trabajamos las validaciones cuando fueron tocadas, entonces
       * para evitar que el usuaria haga un submit del formulario y se rechaze y no aparezaca nada, nosotros hacemos uso,
-      * de 'markAsTouched' para que aparezcan nuestras validaciones.
+      * de 'markAllAsTouched' para que aparezcan nuestras validaciones.
       */
 
-      this.myForm.markAsTouched();
+      this.myForm.markAllAsTouched();
       return;
     }
     console.log(this.myForm.value);
