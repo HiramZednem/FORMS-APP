58e3b0f684592725931c9298e53ebdbcebbfc4df
Hiram Méndez <hirammendez000@gmail.com>
2023-04-29 11:53:39 -0600

    life cycle of reactive forms

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -8,7 +8,7 @@ import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 })
 export class BasicPageComponent {
   // public myForm: FormGroup = new FormGroup({
-  //   //Al inicio se pone el valor por defecto.
+  //   //Al inicio se pone el valor por defecto., el segundo es una unica validacion, o un arreglo de validaciones
   //   name: new FormControl('', [], []),
   //   price: new FormControl(0, [], []),
   //   inStorage: new FormControl(0, [], [])
@@ -16,14 +16,15 @@ export class BasicPageComponent {
 
   //Esta es otra matera de hacerlo
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    price: [0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [0, [ Validators.required, Validators.min(0) ] ],
   })
 
   constructor( private fb: FormBuilder) { }
 
   onSave(): void {
+    if (this.myForm.invalid ) return;
     console.log(this.myForm.value);
   }
 
