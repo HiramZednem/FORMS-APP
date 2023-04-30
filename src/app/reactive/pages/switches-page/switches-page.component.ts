30e91aa5cec3576e87a918320ca69af0d8692a4c
Hiram Méndez <hirammendez000@gmail.com>
2023-04-29 19:59:14 -0600

    switches page conected

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
+
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [ true, Validators.required ],
+    termsAndConditions: [ false, Validators.requiredTrue ]
+
+  });
+
+  public person = {
+    gender: 'F',
+    wantNotification: false
+}
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.myForm.reset(this.person)
+  }
+
+  onSubmit(): void {
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched()
+      return;
+    }
+
+    /* esto es por si mi backend no acepta todas las propiedads, y solo se puedo mandar el genero y si quiere notificaciones,
+    * por que aceptar las politicas es algo mas relacionado con el front, entonces aplico desestructuracion de objetos para
+    * poder pasarle las propiedades que yo requiero
+    */
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value
+    this.person = newPerson;
+    console.log( this.myForm.value );
+    console.log( this.person );
+  }
 
 }
